b6e33422fd6ea23a434b22a6d43c2de30bfa23f1
LuqmanHakim-Kinal <175223015@students.uii.ac.id>
2023-11-23 14:07:39 +0700

    Tambah Pembayaran

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -266,6 +266,16 @@
                             <li><a href="{{ asset('/assets/layout-two-column.html')}}">Two column</a></li>
                         </ul>
                     </li>
+                    <li class="mega-menu mega-menu-sm">
+                        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                            <i class="icon-globe-alt menu-icon"></i><span class="nav-text">Pembayaran</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="#">Data Pembayaran</a></li>
+                            <li><a href="#">Bayar</a></li>
+                            <li><a href="#">Two column</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         </div>
